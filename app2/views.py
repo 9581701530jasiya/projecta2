94e89debf98032feece34248f58c009e544291ca
9581701530jasiya <9581701530jasiya@gmail.com>
2022-11-21 23:14:55 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django. http import HttpResponse
+# Create your views here.
+def jashu(request):
+    return HttpResponse('iam very excited to do jango projects')
